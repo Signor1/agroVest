ae91c8e7d5a1f35f046059abba353223334c84d5
signor1 <emmanuelomemgboji@gmail.com>
2024-08-30 22:19:26 +0000

    feat: added purchase page

--- packages/nextjs/app/user/cart/page.tsx
+++ packages/nextjs/app/user/cart/page.tsx
@@ -1,10 +1,11 @@
 import React from "react"
+import MyCarts from "~~/components/user/MyCarts"
 
 
 export default function UserCarts() {
     return (
         <main className="w-full flex flex-col overflow-x-hidden">
-
+            <MyCarts />
         </main>
     )
 }
